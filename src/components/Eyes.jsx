6a6d9c59b158775664f236b35dca3635e813a4c0
DV1507 <vanjaradhruv@gmail.com>
2024-03-30 17:35:55 +0530

    fix: changed assest directory to public folder

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -3,13 +3,13 @@
 function Eyes() {
   return (
     <div className="eyes w-full h-screen overflow-hidden">
-          <div className="relative w-full h-full bg-cover bg-center bg-[url('assets/images/eyes.png')]">
+      <div className="relative w-full h-full bg-cover bg-center bg-[url('images/eyes.png')]">
         <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-1/2 bg-green-500">
           <div className="w-[15vw] h-[15vw] bg-red-500"></div>
-              </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Eyes
+export default Eyes;
